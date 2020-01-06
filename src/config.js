43759fcbbf8252081ae06edc3e6e592d6d578974
Mustafa Ak <akmustafa@yahoo.com>
2020-01-06 15:41:30 +0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "notes-app-upload-mak1"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://c58t0cd364.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_Y6gMJfjb2",
+      APP_CLIENT_ID: "3b3qcjk95us5uscrh3ifukib3c",
+      IDENTITY_POOL_ID: "eu-central-1:ff59c745-466b-4a2d-9dac-96623c9afab3"
+    }
+  };
